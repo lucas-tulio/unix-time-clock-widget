ed5b734bfbfc2be3dbad4be1e73b450c658a06f1
Lucas Tulio <tulio@commit.to>
2014-04-11 16:05:26 -0300

    Some refactoring and better comments

--- unix-time-clock/src/com/lucasdnd/unixtimeclockwidget/UnixTimeClockProvider.java
+++ unix-time-clock/src/com/lucasdnd/unixtimeclockwidget/UnixTimeClockProvider.java
@@ -1,8 +1,5 @@
 package com.lucasdnd.unixtimeclockwidget;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 import com.lucasdnd.unixtimeclockwidget.R;
 
@@ -13,59 +10,54 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
 import android.widget.RemoteViews;
-
 
 public class UnixTimeClockProvider extends AppWidgetProvider {
 	
-	public static String CLOCK_WIDGET_UPDATE = "com.lucasdnd.unixtimeclockwidget.CLOCK_UPDATE";
-
+	public static String CLOCK_UPDATE = "com.lucasdnd.unixtimeclockwidget.CLOCK_UPDATE";
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
 		super.onReceive(context, intent);
 
-		if (CLOCK_WIDGET_UPDATE.equals(intent.getAction())) {
-
-			// Get the widget manager and ids for this widget provider, then call the shared clock update method.
-			ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
-		    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-		    
+		// Get the widget manager and ids for this widget provider, then call the shared clock update method.
+		ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
+	    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+				    
+		if (CLOCK_UPDATE.equals(intent.getAction())) {
+			
 		    int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
-
 		    for (int appWidgetID: ids) {
-				updateAppWidget(context, appWidgetManager, appWidgetID);
+				updateClock(context, appWidgetManager, appWidgetID);
 		    }
 		}
 	}
-
+	
 	private PendingIntent createClockTickIntent(Context context) {
-    
-		Intent intent = new Intent(CLOCK_WIDGET_UPDATE);
+		Intent intent = new Intent(CLOCK_UPDATE);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
         return pendingIntent;
 	}
-
+	
 	@Override
 	public void onDisabled(Context context) {
 		
 		super.onDisabled(context);
 		
+		// Stop the Timer
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(createClockTickIntent(context));	
 	}
-
+	
 	@Override 
 	public void onEnabled(Context context) {
 		
 		super.onEnabled(context);
 		
+		// Create the Timer
 		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
     	Calendar calendar = Calendar.getInstance();
-        
     	calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.add(Calendar.SECOND, 1);
         alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 1000, createClockTickIntent(context));
@@ -73,17 +65,11 @@ public class UnixTimeClockProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		
-		final int N = appWidgetIds.length;
 
 		// Perform this loop procedure for each App Widget that belongs to this provider
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < appWidgetIds.length; i++) {
 			
 			int appWidgetId = appWidgetIds[i];
-
-			// Create an Intent to launch ExampleActivity
-			Intent intent = new Intent(context, UnixTimeClockActivity.class);
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,	intent, 0);
 
 			// Get the layout for the App Widget and attach an on-click listener to the button
 			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widgetlayout);
@@ -92,15 +78,15 @@ public class UnixTimeClockProvider extends AppWidgetProvider {
 			appWidgetManager.updateAppWidget(appWidgetId, views);
 
 			// Update The clock label using a shared method
-			updateAppWidget(context, appWidgetManager, appWidgetId);
+			updateClock(context, appWidgetManager, appWidgetId);
 		}
 	}
 
-	public static void updateAppWidget(Context context,	AppWidgetManager appWidgetManager, int appWidgetId) {
+	public static void updateClock(Context context,	AppWidgetManager appWidgetManager, int appWidgetId) {
 		
-		RemoteViews updateViews = new RemoteViews(context.getPackageName(),	R.layout.widgetlayout);
-		
-		updateViews.setTextViewText(R.id.clockTextView, "" + (System.currentTimeMillis() / 1000L));
-		appWidgetManager.updateAppWidget(appWidgetId, updateViews);
+		// Update the time text
+		RemoteViews remoteViews = new RemoteViews(context.getPackageName(),	R.layout.widgetlayout);
+		remoteViews.setTextViewText(R.id.clockTextView, "" + (System.currentTimeMillis() / 1000L));
+		appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
 	}
 }
